0afe6c0b9c377f99ea49c6baf5220a4bbd88b6b2
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-14 18:58:15 +0100

    added event emmiter to display recipe detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[] = [
       new Recipe('A Test Recipe', 'This is a simply test',
           'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg'),
-      new Recipe('A Test Recipe', 'This is a simply test',
+      new Recipe('Another Test Recipe', 'This is a simply test',
           'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg')
 
   ];
@@ -19,5 +20,9 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
 
 }
