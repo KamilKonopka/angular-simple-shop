45b894b9e2defd263cae3027bf23b37599cb183e
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-16 15:42:09 +0100

    added functionality to fetch recipes by using routing and params

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
 import {FormsModule} from '@angular/forms';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
       RecipeItemComponent,
       RecipeDetailComponent,
       ShoppingEditComponent,
-      DropdownDirective
+      DropdownDirective,
+      RecipeStartComponent
   ],
   imports: [
     BrowserModule,
