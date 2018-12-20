1d9e7db2e91461fad9fe894ee4624ed732983d6e
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-20 08:52:49 +0100

    restructured services across application - authguard added on recipes module and the rest on core

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,9 @@ import {HeaderComponent} from './header/header.component';
 import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {AuthService} from '../auth/auth.service';
 
 @NgModule({
     declarations: [
@@ -17,6 +20,11 @@ import {AppRoutingModule} from '../app-routing.module';
     exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService,
+        RecipeService,
+        AuthService
     ]
 })
 export class CoreModule {
