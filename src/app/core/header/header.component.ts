0dd66f9be51a6e0081af58c3f97a008cc0d5fee8
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-12-20 08:55:36 +0100

    added authentication method into the header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -29,6 +29,10 @@ export class HeaderComponent {
     onLogout() {
       this.authService.logout();
     }
+
+    isAuthenticated() {
+      return this.authService.isAuthenticated();
+    }
 }
 
 
